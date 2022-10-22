367cc528f3fe0372d16229d36c0a97ecf1a07be4
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-10-23 01:46:21 +0900

    done

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ $("#addTodo").click(function () {
   const inputTodo = $("input").val();
   $("#todoList").append("<li><input type='checkbox'/>" + inputTodo + "</li>");
 });
+
+$(document).on("change", "input[type=checkbox]", function () {
+  if ($(this).is(":checked")) {
+    $(this).parent().css("text-decoration", "line-through");
+  } else {
+    $(this).parent().css("text-decoration", "none");
+  }
+});
